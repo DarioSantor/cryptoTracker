e06d4406866cad7f7f01c1ab6b3b701c35ea4a44
Dario Santos <dario.santos@hotmail.com>
2022-12-29 11:34:23 +0000

    feat: list coins symbols and images

--- Crypto Tracker/Crypto Tracker/CryptoTableViewController.swift
+++ Crypto Tracker/Crypto Tracker/CryptoTableViewController.swift
@@ -14,6 +14,19 @@ class CryptoTableViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CoinData.shared.coins.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let coin = CoinData.shared.coins[indexPath.row]
+        
+        cell.textLabel?.text = coin.symbol
+        cell.imageView?.image = coin.image
+        
+        return cell
+    }
 }
 
